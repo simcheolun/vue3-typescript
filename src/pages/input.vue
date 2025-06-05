5dceae00042d50b0250a7568d5dd50eb7d57ae7b
simcheolun <tokki19771107@gmail.com>
2025-06-05 17:30:07 +0900

    SAVE

--- src/pages/input.vue
+++ src/pages/input.vue
@@ -5,18 +5,19 @@
         <Vue3Info content="type?: 'default' | 'primary' | 'success' | 'warning' | 'danger' | 'info' | 'talk'" />
         <Vue3RowSplit margin="5px" label="" />
         <pre>
-                            {{ `<Vue3Check v-model="modelData" type="..." label="checked"/>` }}
-                </pre>
+                                {{ `<Vue3Check v-model="modelData" type="..." label="checked"/>` }}
+                    </pre>
         <Vue3RowSplit margin="5px" label="" />
         <div class="buttons">
             {{ modelData }}
-            <Vue3Input type="number" v-model="modelData" round="" />
+            <Vue3Input type="number" v-model="modelData" round="" :min="1" :max="10"/>
             <Vue3Input type="text" size="lg" round="5px" />
             <Vue3Input type="text" round="5px" left-icon="fas fa-square-plus" placeholder="please Input" />
-            <Vue3Input type="password" size="sm" round="5px" minLen="1" maxLen="3" left-icon="fas fa-square-plus" placeholder="please Input" />
+            <Vue3Input type="password" size="sm" round="5px" minLen="1" maxLen="3" left-icon="fas fa-square-plus"
+                placeholder="please Input" />
         </div>
     
-
+    
         <Vue3RowSplit margin="20px" label="" />
         <Vue3Info :content="warningEn" />
     
@@ -29,7 +30,7 @@ const warningEn = `※ Warning: This library is distributed under the ISC Licens
 It is provided " AS IS", without any warranties of any kind. The user assumes full responsibility for any
 issues or damages that may arise from using this library. By using this library, you are considered to have
 read and agreed to the ISC License terms. `
-const modelData = ref('111')
+const modelData = ref(1111)
 const types = [
   'default',
   'primary',
